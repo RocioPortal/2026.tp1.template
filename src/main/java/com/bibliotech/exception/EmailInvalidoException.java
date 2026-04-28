9ccf589a90fa4cec29d78e2c4b723cff9068f5c7
Rocio Portal <p.portal@alumno.um.edu.ar>
2026-04-28 13:30:50 -0300

    feat: separate Libro/Ebook, fix exceptions hierarchy and Optional in Prestamo

--- src/main/java/com/bibliotech/exception/EmailInvalidoException.java
+++ src/main/java/com/bibliotech/exception/EmailInvalidoException.java
@@ -1,6 +1,6 @@
 package com.bibliotech.exception;
 
-public class EmailInvalidoException extends RuntimeException {
+public class EmailInvalidoException extends BibliotecaException {
     public EmailInvalidoException(String email) {
         super("El formato del email '" + email + "' no es válido.");
     }
---
+++ src/main/java/com/bibliotech/model/Categoria.java
@@ -0,0 +1,11 @@
+package com.bibliotech.model;
+
+public enum Categoria {
+    CIENCIA,
+    HISTORIA,
+    NOVELA,
+    TECNOLOGIA,
+    ARTE,
+    DERECHO,
+    MEDICINA
+}
